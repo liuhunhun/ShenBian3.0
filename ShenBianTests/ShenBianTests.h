8a650b5ceaca1b71a395c7cb6cb5061b1eb29f55
liuhunhun <waiting_alone_95@sina.com>
2014-01-26 17:25:18 +0800

    体检

---
+++ ShenBianTests/ShenBianTests.h
@@ -0,0 +1,13 @@
+//
+//  ShenBianTests.h
+//  ShenBianTests
+//
+//  Created by 乱柒ハ糟 on 13-12-12.
+//  Copyright (c) 2013年 waiting_alone. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ShenBianTests : SenTestCase
+
+@end
---
+++ ShenBianTests/ShenBianTests.m
@@ -0,0 +1,32 @@
+//
+//  ShenBianTests.m
+//  ShenBianTests
+//
+//  Created by 乱柒ハ糟 on 13-12-12.
+//  Copyright (c) 2013年 waiting_alone. All rights reserved.
+//
+
+#import "ShenBianTests.h"
+
+@implementation ShenBianTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ShenBianTests");
+}
+
+@end
